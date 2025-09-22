a66810bad6e4dcb9a15d8d156c7632053193b22f
Chai-T29 <ctatipigari3@gatech.edu>
2025-09-21 21:17:23 -0700

    Updated epsilon decay strategy

--- dqn/agent.py
+++ dqn/agent.py
@@ -84,7 +84,8 @@ class DQNAgent:
         #self.current_epsilon = max(self.epsilon_end, self.current_epsilon * self._step_decay)
         self.global_step += 1
         fraction = min(1.0, self.global_step / self.epsilon_decay)
-        self.current_epsilon = self.epsilon_start + fraction * (self.epsilon_end - self.epsilon_start)
+        # exponential decay between start and end
+        self.current_epsilon = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * ( (1 - fraction) ** 3)
         return selected_actions.detach()
 
     def compute_loss(self) -> Optional[torch.Tensor]:
