a06004e0c5a877f4727143b23be30602a1827fe9
Chai-T29 <ctatipigari3@gatech.edu>
2025-09-21 22:24:14 -0700

    added back regular replay memory but commented out

--- dqn/replay_memory.py
+++ dqn/replay_memory.py
@@ -70,3 +70,28 @@ def torch_from_numpy(array: np.ndarray):
     import torch  # Local import to avoid hard dependency when unused
 
     return torch.as_tensor(array, dtype=torch.float32)
+
+
+# import random
+# from collections import namedtuple, deque
+
+# Experience = namedtuple('Experience',
+#                         ('state', 'action', 'reward', 'next_state', 'done'))
+
+# class ReplayMemory:
+#     """A cyclic buffer of bounded size that holds the experiences observed recently."""
+
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.memory = deque([], maxlen=capacity)
+
+#     def push(self, *args):
+#         """Save an experience."""
+#         self.memory.append(Experience(*args))
+
+#     def sample(self, batch_size):
+#         """Randomly sample a batch of experiences from memory."""
+#         return random.sample(self.memory, batch_size)
+
+#     def __len__(self):
+#         return len(self.memory)
